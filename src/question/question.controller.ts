b924336ceed253d8d4792b07f4832e39444c9da9
yinkouxi <3405176636@qq.com>
2025-02-17 09:28:03 +0800

    feat: 问卷基本功能

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -7,48 +7,65 @@ import {
   Query,
   HttpException,
   HttpStatus,
+  Post,
+  Delete,
 } from '@nestjs/common';
 
 import { QuestionDto } from './dto/question.dto';
+import { QuestionService } from './question.service';
 
 @Controller('question')
 export class QuestionController {
+  // 依赖注入
+  constructor(private readonly questionService: QuestionService) {}
+
   // 测试
   @Get('test')
   getTest(): string {
     throw new HttpException('测试异常', HttpStatus.BAD_REQUEST);
   }
+
+  // 创建
+  @Post()
+  create() {
+    return this.questionService.create();
+  }
+
+  // 查询全部问卷
   @Get()
-  findAll(
-    @Query('page') page: number,
-    @Query('pageSize') pageSize: number,
-    @Query('keyword') keyword: string,
+  async findAll(
+    @Query('page') page: number = 1,
+    @Query('pageSize') pageSize: number = 10,
+    @Query('keyword') keyword: string = '',
   ) {
-    console.log(page, pageSize, keyword);
+    const list = await this.questionService.findAllList(
+      page,
+      pageSize,
+      keyword,
+    );
+    const count = await this.questionService.countAll(keyword);
     return {
-      list: ['question1', 'question2', 'question3'],
-      count: 3,
+      list,
+      count,
     };
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     console.log(id, 'id');
-    return {
-      id,
-      title: 'question1',
-      desc: 'content1',
-    };
+    return this.questionService.findOne(id);
   }
 
   // 更新
   @Patch(':id')
   updateOne(@Param('id') id: string, @Body() questionDto: QuestionDto) {
     console.log(id, questionDto, 'body');
-    return {
-      id,
-      title: questionDto.title,
-      desc: questionDto.desc,
-    };
+    return this.questionService.update(id, questionDto);
+  }
+
+  // 删除
+  @Delete(':id')
+  deleteOne(@Param('id') id: string) {
+    return this.questionService.delete(id);
   }
 }
--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -1,4 +1,76 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Question } from './schemas/question.schema';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Injectable()
-export class QuestionService {}
+export class QuestionService {
+  constructor(
+    @InjectModel(Question.name) private questionModel: Model<Question>,
+  ) {}
+
+  // 创建问卷
+  async create() {
+    const question = new this.questionModel({
+      title: 'title' + Date.now(),
+      desc: '这是一个问卷描述' + Date.now(),
+    });
+    return await question.save();
+  }
+
+  // 查询单个问卷
+  async findOne(id: string): Promise<Question> {
+    const question = await this.questionModel.findById(id);
+    if (!question) {
+      throw new HttpException('问卷不存在', HttpStatus.NOT_FOUND);
+    }
+    return question;
+  }
+
+  // 删除
+  async delete(id: string) {
+    const question = await this.questionModel.findByIdAndDelete(id);
+    if (!question) {
+      throw new HttpException('问卷不存在', HttpStatus.NOT_FOUND);
+    }
+    return question;
+  }
+
+  // 更新
+  async update(id: string, updateData: Question) {
+    const question = await this.questionModel.findByIdAndUpdate(
+      id,
+      updateData,
+      { new: true },
+    );
+    if (!question) {
+      throw new HttpException('问卷不存在', HttpStatus.NOT_FOUND);
+    }
+    return question;
+  }
+
+  // 查询全部问卷
+  async findAllList(page: number, pageSize: number, keyword: string) {
+    const whereOpt: { title?: { $regex: string; $options: string } } = {};
+    if (keyword) {
+      // 模糊搜索
+      whereOpt.title = { $regex: keyword, $options: 'i' };
+    }
+    return await this.questionModel
+      .find(whereOpt)
+      .sort({ _id: -1 }) // 按id降序排序
+      .skip((page - 1) * pageSize) // 跳过前page-1页
+      .limit(pageSize); // 限制每页显示pageSize条
+  }
+
+  // 查询问卷数量
+  async countAll(keyword: string) {
+    const whereOpt: { title?: { $regex: string; $options: string } } = {};
+    if (keyword) {
+      // 模糊搜索
+      whereOpt.title = { $regex: keyword, $options: 'i' };
+    }
+    return await this.questionModel.countDocuments(whereOpt);
+  }
+}
